420397fbb344c538b607399faaf34d00830f16b1
Tarun Choudhary <tarunchoudhary3884@gmail.com>
2021-06-26 21:51:22 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Info(){
+  return (
+    <div>
+    <div class="note">
+      <h1>HTML</h1>
+      <p>The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.</p>
+    </div>
+    <div class="note">
+      <h1>CSS</h1>
+      <p>Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.</p>
+    </div>
+    <div class="note">
+      <h1>JavaScript</h1>
+      <p>JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.</p>
+    </div>
+    <div class="note">
+      <h1>React</h1>
+      <p>React is an open-source front-end JavaScript library for building user interfaces or UI components. React can be used as a base in the development of mobile applications.</p>
+    </div>
+    </div>
+  );
+}
+export default Info;
